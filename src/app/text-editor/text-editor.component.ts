153c0c67a3669ae6b929e1c3b8fc1e0ed0584a96
Makoni1 <67749844+Makoni1@users.noreply.github.com>
2021-02-25 14:56:52 +0600

    2 box

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-text-editor',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-editor.component.css']
 })
 export class TextEditorComponent implements OnInit {
-
+  @Input() content = "bhbjhbbjnj";
   constructor() { }
-  //wow
+  
   ngOnInit(): void {
-  }
 
+    //wow
+  }
+  
 }
